82390452d27dbabf9a613a93e88ce70e119ac215
Yasmine Hallak <144734453+yasmine2245@users.noreply.github.com>
2026-02-25 16:41:45 -0500

    added payment section and stylist option

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-const booked =[]
+let selectedService = null;
+
 
 function bookService(service){
 	document.getElementById("infoForm").style.display="block";
 	document.getElementById("services").style.display="none";
-	console.log("you picked " + service);
-	booked.push(service);
-	console.log(booked);
+	selectedService = service;
 }
 
-function bookStylist(stylist){
-	document.getElementById("infoForm").style.display="block";
-	document.getElementById("stylists").style.display="none";
-	console.log("you picked " + stylist);
-	booked.push(stylist);
-	console.log(booked);
-
-}
 
 function bookEverything(){
-	if(booked.length == null){
-		alert("You have not booked a service and a stylist");
-	}if (booked.length == 1){
-		alert("You have not booked a service or a stylist");
+
+	event.preventDefault();
+
+	if(!selectedService){
+		alert("Please select a service first.");
+		return;
 	}
 	alert("Your service has been booked.");
 
